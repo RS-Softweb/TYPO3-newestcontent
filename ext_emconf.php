dbda7e791a47b77bd3f88b2e7d273312efcd004a
renestaeker <renestaeker@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-06 20:37:34 +0000

    Added flexform and included static TS
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/newestcontent/trunk@50674 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "newestcontent".
 #
-# Auto generated 04-08-2011 01:55
+# Auto generated 06-08-2011 22:30
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -39,7 +39,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"312e";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"61c4";s:14:"ext_tables.php";s:4:"729d";s:14:"ext_tables.sql";s:4:"4612";s:13:"locallang.xml";s:4:"766e";s:16:"locallang_db.xml";s:4:"97a3";s:19:"doc/wizard_form.dat";s:4:"5207";s:20:"doc/wizard_form.html";s:4:"cb80";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:34:"pi1/class.tx_newestcontent_pi1.php";s:4:"e4d9";s:42:"pi1/class.tx_newestcontent_pi1_wizicon.php";s:4:"7fee";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"a98a";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"9bac";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"61c4";s:14:"ext_tables.php";s:4:"aaa0";s:14:"ext_tables.sql";s:4:"4612";s:12:"flexform.xml";s:4:"a883";s:13:"locallang.xml";s:4:"766e";s:16:"locallang_db.xml";s:4:"e98c";s:19:"doc/wizard_form.dat";s:4:"60ef";s:20:"doc/wizard_form.html";s:4:"c55c";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:34:"pi1/class.tx_newestcontent_pi1.php";s:4:"23ff";s:42:"pi1/class.tx_newestcontent_pi1_wizicon.php";s:4:"7fee";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"a98a";s:20:"static/constants.txt";s:4:"931c";s:16:"static/setup.txt";s:4:"c88c";}',
+	'suggests' => array(
+	),
 );
 
 ?>
