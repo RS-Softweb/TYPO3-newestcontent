7908bc2d5995db91eaa3e63f36fb9a66fcd27a6a
renestaeker <renestaeker@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-17 13:06:27 +0000

    (Issue #38141) :  Cleanup and switch to Extbase/Fluid
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/newestcontent/trunk@63515 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++
@@ -1,8 +0,0 @@
-#
-# Table structure for table 'tt_content'
-#
-CREATE TABLE tt_content (
-	tx_newestcontent_publishnew tinyint(3) DEFAULT '0' NOT NULL,
-	tx_newestcontent_publishdate int(11) DEFAULT '0' NOT NULL,
-	tx_newestcontent_shorttext text
-);
